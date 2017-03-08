01bf50d6db54cfc5ac30613fbc549eaded10c532
潘安 <wslongchen@vip.qq.com>
2017-03-08 17:31:03 +0800

    项目初始化
    。。。

---
+++ app/src/main/java/cn/mrpan/redpackhelper/activity/SettingsActivity.java
@@ -0,0 +1,89 @@
+package cn.mrpan.redpackhelper.activity;
+
+import android.annotation.TargetApi;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+import android.provider.Settings;
+import android.support.v4.app.FragmentActivity;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import cn.mrpan.redpackhelper.R;
+import cn.mrpan.redpackhelper.fragment.CommentSettingsFragment;
+import cn.mrpan.redpackhelper.fragment.GeneralSettingsFragment;
+
+/**
+ * Created by Mr.Pan on 2017/3/8.
+ */
+
+public class SettingsActivity extends FragmentActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_preferences);
+
+        loadUI();
+        prepareSettings();
+    }
+
+    private void prepareSettings() {
+        String title, fragId;
+        Bundle bundle = getIntent().getExtras();
+        if (bundle != null) {
+            title = bundle.getString("title");
+            fragId = bundle.getString("frag_id");
+        } else {
+            title = getString(R.string.preference);
+            fragId = "GeneralSettingsFragment";
+        }
+
+        TextView textView = (TextView) findViewById(R.id.settings_bar);
+        textView.setText(title);
+
+        FragmentManager fragmentManager = getFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+
+        if ("GeneralSettingsFragment".equals(fragId)) {
+            fragmentTransaction.replace(R.id.preferences_fragment, new GeneralSettingsFragment());
+        } else if ("CommentSettingsFragment".equals(fragId)) {
+            fragmentTransaction.replace(R.id.preferences_fragment, new CommentSettingsFragment());
+        }
+        fragmentTransaction.commit();
+    }
+
+    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+    private void loadUI() {
+        if (android.os.Build.VERSION.SDK_INT < Build.VERSION_CODES.LOLLIPOP) return;
+
+        Window window = this.getWindow();
+
+        window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+
+        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+
+        window.setStatusBarColor(getResources().getColor(R.color.statusBar));
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    public void performBack(View view) {
+        super.onBackPressed();
+    }
+
+    public void enterAccessibilityPage(View view) {
+        Toast.makeText(this, getString(R.string.turn_on_toast), Toast.LENGTH_SHORT).show();
+        Intent mAccessibleIntent =
+                new Intent(Settings.ACTION_ACCESSIBILITY_SETTINGS);
+        startActivity(mAccessibleIntent);
+    }
+}
+
